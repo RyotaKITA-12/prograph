6719d33cdef27c8077cc5a4ff7f38d3d1824324d
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-11-01 08:47:41 +0900

    add dijkstra-path

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -54,10 +54,10 @@
         </div>
 
         <v-network-graph v-model:selected-nodes="selectedNodes" v-model:selected-edges="selectedEdges" ref="graph"
-            class="graph" :nodes="nodes" :edges="edges" :layouts="layouts" :configs="configs"
+            class="graph" :paths="paths" :nodes="nodes" :edges="edges" :layouts="layouts" :configs="configs"
             :event-handlers="eventHandlers">
             <template #edge-label="{ edge, ...slotProps }">
-                <v-edge-label :text="edge.cost" align="center" vertical-align="above" v-bind="slotProps" />
+                <v-edge-label :text="`${edge.cost}`" align="center" vertical-align="below" v-bind="slotProps" />
             </template>
         </v-network-graph>
         <div ref="viewMenu" class="context-menu">Menu for the background</div>
@@ -94,7 +94,7 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, onMounted, ref } from "vue"
+import { reactive, onMounted, ref, watchEffect } from "vue"
 import * as vNG from "v-network-graph"
 import { Nodes, Edges, Layouts, GridLayout, EventHandlers } from "v-network-graph"
 //@ts-ignore
@@ -109,17 +109,176 @@ import {
     ZoomOut,
     CaretRight,
     CaretBottom,
-    CircleCloseFilled,
 } from '@element-plus/icons-vue'
+
+type GraphMap = { [key: string]: { [key: string]: number } }
+type EdgeMap = { [key: string]: { [key: string]: string } }
+
+class Graph {
+    map: GraphMap
+    edgeMap: EdgeMap
+
+    _sorter = function (a: string, b: string) {
+        return parseFloat(a) - parseFloat(b)
+    }
+
+    constructor(edges: Edges) {
+        // var map = {a:{b:3,c:1},b:{a:2,c:1},c:{a:4,b:1}},
+        const map: GraphMap = {}
+        const edgeMap: EdgeMap = {}
+        Object.entries(edges).forEach(([edgeId, edge]) => {
+            const source = edge.source
+            const target = edge.target
+            const cost = Number(edge.cost)
+            if (!map[source]) map[source] = {}
+            if (!map[target]) map[target] = {}
+            if (!edgeMap[source]) edgeMap[source] = {}
+            if (!edgeMap[target]) edgeMap[target] = {}
+
+            if (map[source][target]) {
+                if (map[source][target] > cost) {
+                    map[source][target] = cost
+                    map[target][source] = cost
+                    edgeMap[source][target] = edgeId
+                    edgeMap[target][source] = edgeId
+                }
+            } else {
+                map[source][target] = cost
+                map[target][source] = cost
+                edgeMap[source][target] = edgeId
+                edgeMap[target][source] = edgeId
+            }
+        })
+        this.map = map
+        this.edgeMap = edgeMap
+    }
+
+    findShortestPath(viaNodes: string[]) {
+        return this._findShortestPath(this.map, viaNodes)
+    }
+    convertNodesToEdges(nodes: string[]): string[] {
+        const edges: string[] = []
+        if (nodes.length === 0) {
+            return []
+        }
+        let prev = nodes[0]
+        for (let i = 1; i < nodes.length; i++) {
+            const next = nodes[i]
+            edges.push(this.edgeMap[prev][next])
+            prev = next
+        }
+        return edges
+    }
+
+    _extractKeys(obj: object) {
+        const keys = []
+        let key
+        for (key in obj) {
+            Object.prototype.hasOwnProperty.call(obj, key) && keys.push(key)
+        }
+        return keys
+    }
+
+    _findPaths(map: GraphMap, start: string, end: string) {
+        const costs: { [key: string]: number } = {}
+        const open: { [key: string]: string[] } = { 0: [start] }
+        const predecessors: { [key: string]: string } = {}
+        let keys
+
+        const addToOpen = function (cost: number, vertex: string) {
+            const key = "" + cost
+            if (!open[key]) {
+                open[key] = []
+            }
+            open[key].push(vertex)
+        }
+
+        costs[start] = 0
+
+        // eslint-disable-next-line no-unmodified-loop-condition
+        while (open) {
+            if (!(keys = this._extractKeys(open)).length) {
+                break
+            }
+
+            keys.sort(this._sorter)
+
+            const key = keys[0]
+            const bucket = open[key]
+            const node = bucket.shift() || ""
+            const currentCost = parseFloat(key)
+            const adjacentNodes = map[node] || {}
+
+            if (!bucket.length) {
+                delete open[key]
+            }
+
+            for (const vertex in adjacentNodes) {
+                if (Object.prototype.hasOwnProperty.call(adjacentNodes, vertex)) {
+                    const cost = adjacentNodes[vertex]
+                    const totalCost = cost + currentCost
+                    const vertexCost = costs[vertex]
+
+                    if (vertexCost === undefined || vertexCost > totalCost) {
+                        costs[vertex] = totalCost
+                        addToOpen(totalCost, vertex)
+                        predecessors[vertex] = node
+                    }
+                }
+            }
+        }
+
+        if (costs[end] === undefined) {
+            return null
+        } else {
+            return predecessors
+        }
+    }
+
+    _extractShortest(predecessors: { [key: string]: string }, end: string) {
+        const nodes = []
+        let u = end
+
+        while (u !== undefined) {
+            nodes.push(u)
+            u = predecessors[u]
+        }
+
+        nodes.reverse()
+        return nodes
+    }
+
+    _findShortestPath(map: GraphMap, nodes: string[]) {
+        nodes = [...nodes] // copy
+        let start = nodes.shift() || ""
+        let end: string
+        let predecessors
+        const path: string[] = []
+        let shortest
+
+        while (nodes.length) {
+            end = nodes.shift() || ""
+            predecessors = this._findPaths(map, start, end)
+
+            if (predecessors) {
+                shortest = this._extractShortest(predecessors, end)
+                if (nodes.length) {
+                    path.push.apply(path, shortest.slice(0, -1))
+                } else {
+                    return path.concat(shortest)
+                }
+            } else {
+                return null
+            }
+
+            start = end
+        }
+    }
+}
+
 
 const visible = ref(false)
 
-const urls = [
-    "@/assets/tutorial/001.png",
-    "@/assets/tutorial/002.png",
-    "@/assets/tutorial/003.png",
-    "@/assets/tutorial/004.png",
-]
 
 const nodes: Nodes = reactive({})
 const edges: Edges = reactive({})
@@ -166,6 +325,14 @@ configs.edge.marker = {
         units: "strokeWidth",
         color: null,
     }
+}
+configs.path = {
+    visible: true,
+    curveInNode: true,
+    path: {
+        width: 14,
+        color: "#ff000088",
+    },
 }
 configs.view.boxSelectionEnabled = true
 configs.view.layoutHandler = new GridLayout({ grid: 15 })
@@ -249,7 +416,7 @@ function removeNode() {
 
 function addEdge() {
     if (selectedNodes.value.length !== 2) return
-    const [source, target, cost, name] = [...selectedNodes.value, '0', 'edge']
+    const [source, target, cost, name] = [...selectedNodes.value, 1, 'edge']
     const edgeId = `edge${nextEdgeIndex.value}`
     edges[edgeId] = { source, target, cost, name }
     nextEdgeIndex.value++
@@ -313,6 +480,18 @@ function showEdgeContextMenu(params: vNG.EdgeEvent<MouseEvent>) {
     }
 }
 
+const paths = ref<vNG.Paths>({})
+
+const targetNode = ref("node2")
+watchEffect(() => {
+    const graph = new Graph(edges)
+    const routeOfNodes = graph.findShortestPath(["node1", targetNode.value])
+    if (routeOfNodes) {
+        const routeOfEdges = graph.convertNodesToEdges(routeOfNodes)
+        paths.value = { shortestPath: { edges: routeOfEdges } }
+    }
+})
+
 const eventHandlers: EventHandlers = {
     "view:mode": mode => {
         // Observe mode change events
@@ -320,6 +499,10 @@ const eventHandlers: EventHandlers = {
     },
     "node:contextmenu": showNodeContextMenu,
     "edge:contextmenu": showEdgeContextMenu,
+    "node:pointerover": ({ node }) => {
+        if (node === "node1") return
+        targetNode.value = node
+    },
 }
 </script>
 
